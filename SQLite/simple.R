9e97ca5d46f3554d3c58a91a1906cd9b67a4c26d
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2014-09-29 11:03:22 -0500

    Basic R/Python SQLite access

---
+++ SQLite/simple.R
@@ -0,0 +1,19 @@
+# Simple RSQlite.
+
+# install.packages("RSQLite")
+# setwd("~/GitHub/CompOpt-2014-Fall/SQLite")
+
+library(DBI)
+library(RSQLite)
+
+drv <- dbDriver("SQLite")
+db <- dbConnect(drv, dbname="simple.db")
+
+d <- dbGetQuery(db,"SELECT key,value FROM Data")
+
+summary(d)
+sum(d$value)
+
+# close
+sqliteCloseConnection(db)
+sqliteCloseDriver(drv)
---
+++ SQLite/simple.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+## Simple database.  Overwrite existing database file simple.db
+
+import sqlite3
+import os
+from TDDUtils import *
+
+class Database:
+    def __init__(self,database='simple.db'):
+        if os.path.exists(database):
+            os.unlink(database)
+        schema=open('simple.sql').read()
+        #print(schema)
+        self.con=sqlite3.connect(database)
+        self.con.execute(schema)
+    def put(self,key,value):
+        statement='INSERT INTO Data (key,value) VALUES (?,?)'
+        cur=self.con.execute(statement,(key,value))
+        cur.close()
+        return True
+
+    def get(self,key):
+        statement='SELECT value FROM Data WHERE key=?'
+        cur=self.con.execute(statement, (key,))
+        value=cur.fetchone()
+        cur.close()
+        return value[0]
+
+    def close(self):
+        self.con.commit()
+        self.con.close()
+
+def test():
+    db=Database()
+    assertTrue(db.put('one',1),"Add first entry")
+    assertEquals('1',db.get('one'),"Verify write")
+    assertTrue(db.put('two',2),"Add second entry")
+    db.close()
+
+if __name__=='__main__':
+    print("simple.py>")
+    test()
+    
