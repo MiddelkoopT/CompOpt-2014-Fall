a38ca133923f92903a9f5c52f9f94e12a2aca7d8
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2014-10-27 13:50:18 -0500

    In class experiments database

---
+++ SQLite/experiments.sql
@@ -0,0 +1,27 @@
+DROP TABLE Experiments IF EXISTS
+CREATE TABLE Experiments (
+  EID INTEGER PRIMARY KEY,
+  alpha DOUBLE,
+  beta DOUBLE,
+  epsilon DOUBLE
+);
+
+INSERT INTO Experiments (EID,alpha,beta,epsilon) VALUES (1,3,6,0.2);
+INSERT INTO Experiments (EID,alpha,beta,epsilon) VALUES (2,3,9,0.2);
+INSERT INTO Experiments (EID,alpha,beta,epsilon) VALUES (4,6,4.0.1);
+    
+DROP TABLE RUNS IF EXISTS
+CREATE TABLE Runs (
+  EID INTEGER,
+  RUNID INTEGER PRIMARY KEY,
+  solution DOUBLE,
+  start DATETIME,
+  finish DATETIME
+);
+
+INSERT INTO Runs (EID,RUNID) VALUES (1,1);
+INSERT INTO Runs (EID,RUNID) VALUES (1,2);
+INSERT INTO Runs (EID,RUNID) VALUES (1,3);
+INSERT INTO Runs (EID,RUNID) VALUES (2,4);
+INSERT INTO Runs (EID,RUNID) VALUES (2,5);
+INSERT INTO Runs (EID,RUNID) VALUES (2,6);
